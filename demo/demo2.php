23333a445f4b2a76230608445dedd51944850d5d
guofeng <guofeng@pexdb.com>
2020-02-03 22:13:45 +0800

    finish commit

--- demo/demo2.php
+++ demo/demo2.php
@@ -4,12 +4,12 @@ require '../src/PhpXirr.php';
 use phpXirr\PhpXirr;
 
 $data = [
-    ['date' => '2017-04-30', 'payment' => '1000000'],
+    ['date' => '2017-04-30', 'payment' => '-1000000'],
     ['date' => '2019-01-31', 'payment' => '1'],
 ];
 
 $PhpXirr = new PhpXirr();
 $PhpXirr->XirrData($data);
 $interest = $PhpXirr->getXirr();
-echo $interest; // 0.084870
+echo $interest;
 exit;
